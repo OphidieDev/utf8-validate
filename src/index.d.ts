8ea3de29b8527a4c18388dc91230b672488b97b7
OphidieDev <83857517+OphidieDev@users.noreply.github.com>
2025-05-26 04:37:23 -0400

    feat: update grammar in index.d.ts

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,5 +1,5 @@
 /**
-	* Validates whether a given string contains utf8 characters.
+	* Validates whether a given string contains only utf8 characters.
     *
     * @param input The string to validate
     * @returns {boolean} Whether or not the string is valid utf8
