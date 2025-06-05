10f379a19a45c82282d35c14872f455eac3281f9
OphidieDev <83857517+OphidieDev@users.noreply.github.com>
2025-06-05 01:50:24 -0400

    fix!: index typescript function declaration

--- src/index.d.ts
+++ src/index.d.ts
@@ -4,4 +4,6 @@
     * @param input The string to validate
     * @returns {boolean} Whether or not the string is valid utf8
 */
-export function utf8_validate(input: string): boolean;
+declare function utf8_validate(input: string): boolean;
+
+export = utf8_validate
